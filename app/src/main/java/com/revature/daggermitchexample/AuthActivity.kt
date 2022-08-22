d526c53b1c9101808eefa1af9ad7664bdab8b279
RyanKCox <ryankcox90@gmail.com>
2022-08-22 15:51:08 -0500

    Created AuthActivity Screen and used glide to load logo

--- app/src/main/java/com/revature/daggermitchexample/AuthActivity.kt
+++ app/src/main/java/com/revature/daggermitchexample/AuthActivity.kt
@@ -1,23 +1,30 @@
 package com.revature.daggermitchexample
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.RequestManager
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
 class AuthActivity : DaggerAppCompatActivity() {
 
     @Inject
-    lateinit var testString:String
-
-    var isApp = false
-        @Inject set
+    lateinit var logo:Drawable
+    @Inject
+    lateinit var requestManager: RequestManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        Log.d("AuthActivity: ",testString)
-        Log.d("AuthActivity",isApp.toString())
+        setLogo()
+
+    }
+    private fun setLogo(){
+        requestManager
+            .load(logo)
+            .into(findViewById<ImageView>(R.id.login_logo))
     }
 }
--- app/src/main/java/com/revature/daggermitchexample/di/module/AppModule.kt
+++ app/src/main/java/com/revature/daggermitchexample/di/module/AppModule.kt
@@ -1,6 +1,12 @@
 package com.revature.daggermitchexample.di.module
 
 import android.app.Application
+import android.graphics.drawable.Drawable
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import com.revature.daggermitchexample.R
 import dagger.Module
 import dagger.Provides
 
@@ -8,10 +14,20 @@ import dagger.Provides
 class AppModule {
 
     @Provides
-    fun someString() = "This is a test String"
-
+    fun provideRequestOptions():RequestOptions{
+        return RequestOptions.placeholderOf(R.drawable.white_background)
+            .error(R.drawable.white_background)
+    }
     @Provides
-    fun getApp(app:Application):Boolean{
-        return app != null
+    fun provideRequestManager(
+        app:Application,
+        requestOptions: RequestOptions
+    ):RequestManager{
+        return Glide.with(app)
+            .setDefaultRequestOptions(requestOptions)
+    }
+    @Provides
+    fun provideAppDrawable(app:Application): Drawable {
+        return ContextCompat.getDrawable(app,R.drawable.logo)!!
     }
 }
